20e9b4b776c3b68586a05971ea23803614f33d70
Jan Rapp <3jr@users.noreply.github.com>
2011-09-10 21:46:26 +0000

    Continue

---
+++ BallOnTiltablePlate2/JanRapp/SimulationIO.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using BallOnTiltablePlate;
+
+namespace JanRapp
+{
+    /// <summary>
+    /// Interaction logic for SimulationIO.xaml
+    /// </summary>
+    public partial class SimulationIO : Grid, IBallInput, IPlateOutput, IBallOnPlatePart
+    {
+        public FrameworkElement Settings
+        {
+            get { return this; }
+        }
+
+        public string AuthorFirstName
+        {
+            get { return "Jan"; }
+        }
+
+        public string AuthorLastName
+        {
+            get { return "Rapp"; }
+        }
+
+        public Version Version
+        {
+            get { return new Version(0, 1); }
+        }
+
+        public string PartName
+        {
+            get { return "SimulationIO"; }
+        }
+
+        public SimulationIO()
+        {
+            InitializeComponent();
+        }
+
+        public Action<Point> CallOnNewPoint
+        {
+            set { throw new NotImplementedException(); }
+        }
+
+        public Vector Tilt
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
