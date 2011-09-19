eae35412d1d90f097c51f8b30d417e3c49f6f14f
FlySoft_cp <SND\FlySoft_cp>
2011-09-19 14:25:51 +0000

    Emgu-CV - kleiner fortschritt

--- BallOnTiltablePlate2/BallOnTiltablePlate/MoritzUehling/Kinect/OpenCVManager.cs
+++ BallOnTiltablePlate2/BallOnTiltablePlate/MoritzUehling/Kinect/OpenCVManager.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace BallOnTiltablePlate.MoritzUehling.Kinect
 {
     class OpenCVManager
     {
-        public List<Point> GetPoints(Bitmap data)
+        public Bitmap GetPoints(Bitmap data)
         {
-            /* Image<Bgr,byte> img  = new Image<Bgr,byte>(data);
-            Image<Gray, Byte> gray = img.Convert<Gray, Byte>().PyrDown().PyrUp(); */
+            Image<Bgr,byte> img  = new Image<Bgr,byte>(data);
+            Image<Gray, Byte> gray = img.Convert<Gray, Byte>().PyrDown().PyrUp();
+
+            img.ToBitmap();
 
             return null;
         }
