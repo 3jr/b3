26db26aca9b4aa042c6c75d4d204eae462cff94a
FlySoft_cp <SND\FlySoft_cp>
2011-09-19 15:31:38 +0000

    Emgu.CV rausgeschmissen. Ließ sich mit biegen und Brechen nicht einbinden.

--- BallOnTiltablePlate2/BallOnTiltablePlate/MoritzUehling/Kinect/OpenCVManager.cs
+++ BallOnTiltablePlate2/BallOnTiltablePlate/MoritzUehling/Kinect/OpenCVManager.cs
@@ -3,21 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
-using Emgu.CV;
-using Emgu.CV.Structure;
-
 namespace BallOnTiltablePlate.MoritzUehling.Kinect
 {
     class OpenCVManager
     {
         public Bitmap GetPoints(Bitmap data)
         {
-            Image<Bgr,byte> img  = new Image<Bgr,byte>(data);
-            Image<Gray, Byte> gray = img.Convert<Gray, Byte>().PyrDown().PyrUp();
 
-            img.ToBitmap();
 
-            return null;
+            return data;
         }
     }
 }
