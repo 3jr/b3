7f4155a8a538abbbe29c88efa1b956e68d81eb48
Jan Rapp <3jr@users.noreply.github.com>
2011-10-03 10:03:31 +0000

    Deleated that File again and added it as .xaml and .xaml.cs again

---
+++ BallOnTiltablePlate2/BallOnTiltablePlate/JanRapp/Output/BasicPlateOutput.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BallOnTiltablePlate.JanRapp.Output
+{
+    /// <summary>
+    /// Interaction logic for BasicPlateOutput.xaml
+    /// </summary>
+    public partial class BasicPlateOutput : UserControl
+    {
+        public BasicPlateOutput()
+        {
+            InitializeComponent();
+        }
+    }
+}
