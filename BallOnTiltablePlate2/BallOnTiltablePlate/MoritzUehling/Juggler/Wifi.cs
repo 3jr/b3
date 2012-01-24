80a6d5d256af0d5bdb0a670b6078d4c50e2cbd4c
FlySoft_cp <SND\FlySoft_cp>
2012-01-24 21:29:25 +0000

    Jetzt kann mit dem WP7 gesteuert werden :) Die App füge ich mal nicht als Projekt hinzu, vielleicht morgen...

--- BallOnTiltablePlate2/BallOnTiltablePlate/MoritzUehling/Juggler/Wifi.cs
+++ BallOnTiltablePlate2/BallOnTiltablePlate/MoritzUehling/Juggler/Wifi.cs
@@ -14,18 +14,24 @@ using System.Windows.Shapes;
 using BallOnTiltablePlate.JanRapp.MainApp.Helper;
 using BallOnTiltablePlate.JanRapp.Utilities;
 using BallOnTiltablePlate;
+using BallOnTiltablePlate.MoritzUehling.Helpers;
 
 
 /// <summary>
 /// Interaction logic for BasicBalance.xaml
 /// </summary>
-[BallOnPlateItemInfo("Moritz", "Uehling", "BallCircle", "0.1")]
-public class CircleJuggler : UserControl, IJuggler<BallOnTiltablePlate.JanRapp.Preprocessor.IBasicPreprocessor>
+[BallOnPlateItemInfo("Moritz", "Uehling", "Phone Controller", "0.8")]
+public class WifiConnector : UserControl, IJuggler<BallOnTiltablePlate.JanRapp.Preprocessor.IBasicPreprocessor>
 {
 	double veloFactor = 0;
 	double posFactor = 1;
 
 	double factor = 0.1;
+
+	WifiHelper connector;
+
+
+	public BallOnTiltablePlate.JanRapp.Preprocessor.IBasicPreprocessor IO { private get; set; }
 
 	#region Base
 	public System.Windows.FrameworkElement SettingsUI
@@ -34,49 +40,31 @@ public class CircleJuggler : UserControl, IJuggler<BallOnTiltablePlate.JanRapp.P
 	}
 	#endregion
 
-	public BallOnTiltablePlate.JanRapp.Preprocessor.IBasicPreprocessor IO { private get; set; }
-
 	public void Start()
 	{
+
 	}
 
 	public void Stop()
 	{
 	}
 
-	public CircleJuggler()
+	public WifiConnector()
 	{
+		connector = new WifiHelper();
 	}
 
 	public void Update()
 	{
 		if (!IO.Position.HasNaN())
 		{
-			Vector velo = IO.Velocity;
-
-			if (velo.HasNaN())
-				velo = new Vector(0, 0);
-
-
-
-
-			Vector tilt = IO.Position * posFactor;
-
-
-			//double angle = Math.Atan(tilt.X / tilt.Y);
-			//angle += Math.PI / 2; //+90°
-
-			
-			//Vector tilt2 = new Vector(Math.Cos(angle), Math.Sign(angle)) * Math.Sqrt(Math.Pow(tilt.X, 2) + Math.Pow(tilt.Y,2));
-
-			//tilt *= posFactor;
-			//tilt -= tilt2 * veloFactor;
-
-			////tilt += velo;
-
-			tilt *= factor;
-
-			IO.SetTilt(tilt);
+			connector.WritePos(IO.Position.X, IO.Position.Y);
 		}
+		else
+		{
+			connector.WritePos(0, 0);
+		}
+
+		IO.SetTilt(new Vector(-connector.tiltY, connector.tiltX));
 	}
 }
---
+++ BallOnTiltablePlate2/BallOnTiltablePlate/MoritzUehling/Juggler/WifiHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+using System.Threading;
+
+namespace BallOnTiltablePlate.MoritzUehling.Helpers
+{
+	class WifiHelper
+	{
+
+		TcpListener listener;
+		TcpClient phone;
+
+		StreamReader read;
+		StreamWriter write;
+
+		Thread reciveThread;
+
+		public double tiltX = 0;
+		public double tiltY = 0;
+
+		public WifiHelper()
+		{
+			listener = new TcpListener(IPAddress.Any, 31337);
+
+			listener.Start();
+			listener.BeginAcceptTcpClient(new AsyncCallback(PhoneConnected), listener);
+
+		}
+
+		private void PhoneConnected(IAsyncResult result)
+		{
+			phone = listener.EndAcceptTcpClient(result);
+
+			read = new StreamReader(phone.GetStream());
+			write = new StreamWriter(phone.GetStream());
+
+
+			Console.WriteLine("Connected, saying hello");
+
+			write.WriteLine("balance");
+			write.Flush();
+
+
+			string line = read.ReadLine();
+			if (line.StartsWith("wtf?"))
+			{
+				Console.WriteLine("Phone accepted!");
+			}
+
+			reciveThread = new Thread(new ThreadStart(ReciveData));
+			reciveThread.Start();
+
+		}
+
+		public void WritePos(double x, double y)
+		{
+			if (write != null)
+			{
+				write.Write(String.Format("{0}|{1}", x, y));
+				write.Flush();
+			}
+		}
+
+		private void ReciveData()
+		{
+			while (true)
+			{
+				while (phone.Available == 0)
+				{
+					Thread.Sleep(5);
+				}
+
+				string message = read.ReadLine();
+
+				string[] vars = message.Split("|".ToCharArray());
+
+
+				tiltX = double.Parse(vars[0]);
+				tiltY = double.Parse(vars[1]);
+			}
+		}
+		
+	}
+}
